3298e55a410d5333b2465e6475fb464277655654
linmalin <linmalin@yahoo.com>
2013-08-28 12:05:38 -0700

    Galleries and grids
    Added grid, gallery.
    Added images.
    Included clearfix and box sizing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function($) {
 
+//to make out grid pretty
+$('.posts').masonry({
+  //columnWidth: 200,
+  itemSelector: '.post'
+});
+$("#slides").slidesjs({
+	navigtion : {
+		effect : "fade"
+	}
+
+	});
 });
 
 
